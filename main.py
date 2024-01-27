91e33c4c9a0bf7ebb7065cf57f3aa1be74f64731
Hama-2009 <146738659+Hama-2009@users.noreply.github.com>
2024-01-26 19:11:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,28 @@ class AddContactDialog(QDialog):
         cancel_button.clicked.connect(self.reject)
         buttons_layout.addWidget(cancel_button)
 
+
+            def create_label_and_input(self, label_text, input_widget):
+        container_widget = QWidget()
+        container_layout = QHBoxLayout(container_widget)
+
+        label = QLabel(label_text)
+        label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        container_layout.addWidget(label)
+
+        container_layout.addWidget(input_widget)
+        return container_widget
+
+    def get_contact_info(self):
+        return self.name_edit.text(), self.surname_edit.text(), self.phone_edit.text(), self.email_edit.text()
+
+    def set_contact_info(self, name, surname, phone, email):
+        self.name_edit.setText(name)
+        self.surname_edit.setText(surname)
+        self.phone_edit.setText(phone)
+        self.email_edit.setText(email)
+
+
+
         layout.addLayout(buttons_layout)
         self.setLayout(layout)
